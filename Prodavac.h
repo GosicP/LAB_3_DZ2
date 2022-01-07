5246f2b0d78334007d78cbc2395eaca17c2ccc47
Pavle Gosic <pvlgsc@gmail.com>
2022-01-07 20:42:54 +0100

    Update Prodavac.h

--- Prodavac.h
+++ Prodavac.h
@@ -21,7 +21,7 @@ private:
 
 
 
-    Lista<tip_podatka_lista> lista_katalog;
+    Lista<tip_podatka_lista*> lista_katalog;
 
     double prodajna_cena_artikla=0;
 
@@ -39,18 +39,18 @@ public:
     }
 
     void dodajUKatalog(Artikal& a, double marge, int dani_nabavka){
-        tip_podatka_lista katalog;
-        katalog.artikal=a;
-        katalog.marza=marge;
-        katalog.broj_dana_za_nabavku=dani_nabavka;
+        tip_podatka_lista *katalog;
+        katalog->artikal=a;
+        katalog->marza=marge;
+        katalog->broj_dana_za_nabavku=dani_nabavka;
         lista_katalog+=katalog;
     }
 
     void obrada(Posiljka& p){
         for(int i=0; i<lista_katalog.dohvBrojUListi(); i++){
-            if(lista_katalog[i].artikal==p.getArtikal()){
-                p.detalj.broj_dana_cekanja=p.detalj.broj_dana_cekanja+lista_katalog[i].broj_dana_za_nabavku;
-                prodajna_cena_artikla=lista_katalog[i].artikal.getRealnaNabavnaCena()*lista_katalog[i].marza;
+            if(lista_katalog[i]->artikal==p.getArtikal()){
+                p.detalj.broj_dana_cekanja=p.detalj.broj_dana_cekanja+lista_katalog[i]->broj_dana_za_nabavku;
+                prodajna_cena_artikla=lista_katalog[i]->artikal.getRealnaNabavnaCena()*lista_katalog[i]->marza;
                 p.detalj.cena_posiljke=p.detalj.cena_posiljke+prodajna_cena_artikla;
             }
         }
